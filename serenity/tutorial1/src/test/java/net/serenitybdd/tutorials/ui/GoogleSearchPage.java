549a0735e6464e31eae9486488f0a2c40af71019
cyrillebonnaud <bonnaudc@gmail.com>
2016-12-02 15:17:32 +0100

    dirty commit
    Signed-off-by: cyrillebonnaud <bonnaudc@gmail.com>

--- serenity/tutorial1/src/test/java/net/serenitybdd/tutorials/ui/GoogleSearchPage.java
+++
@@ -1,7 +0,0 @@
-package net.serenitybdd.tutorials.ui;
-
-import net.serenitybdd.core.pages.PageObject;
-import net.thucydides.core.annotations.DefaultUrl;
-
-@DefaultUrl("http://www.google.co.uk")
-public class GoogleSearchPage extends PageObject {}
